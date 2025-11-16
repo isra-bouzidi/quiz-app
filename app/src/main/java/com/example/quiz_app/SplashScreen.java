9db8da5c7ffbc83e957b44b423fb80544f948295
isra-bouzidi <b.isra2004@gmail.com>
2025-11-16 09:42:35 +0100

    QA-4: signUp button declarations fields

--- app/src/main/java/com/example/quiz_app/SplashScreen.java
+++ app/src/main/java/com/example/quiz_app/SplashScreen.java
@@ -47,7 +47,7 @@ public class SplashScreen extends AppCompatActivity {
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run(){
-                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                Intent intent = new Intent(SplashScreen.this, SignUp.class);
                 startActivity(intent);
                 finish();
             }
