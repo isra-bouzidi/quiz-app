82feaca8e21710288acad7d46fef13e4bbf25ab7
Isra Bouzidi <b.isra2004@gmail.com>
2025-11-16 09:46:10 +0100

    QA-4: signUp button (#4)
    declarations
    fields

--- app/src/main/java/com/example/quiz_app/SignIn.java
+++ app/src/main/java/com/example/quiz_app/SignIn.java
@@ -11,28 +11,21 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class SignUp extends AppCompatActivity {
+public class SignIn extends AppCompatActivity {
 
-    EditText userName, userEmail, userPhone, password;
-    Button register;
-    TextView signIn;
+    EditText  userEmail, password;
+    Button signIn;
+    TextView signUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_sign_up);
+        setContentView(R.layout.activity_sign_in);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        userName = findViewById(R.id.userName);
-        userEmail = findViewById(R.id.userEmail);
-        userPhone = findViewById(R.id.userPhone);
-        password = findViewById(R.id.password);
-        register = findViewById(R.id.register);
-        signIn = findViewById(R.id.signIn);
     }
 }
